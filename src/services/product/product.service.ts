e62a05da122d57fa0560d227c2f87f130ff391a9
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-05 20:43:02 +0200

    all the fetch

--- src/services/product/product.service.ts
+++ src/services/product/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
+import { Product } from '../../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,27 @@ export class ProductService {
   //get all the products
   getProducts(): Observable<any> {
     return this.http.get('http://localhost/backend/controller/view.php')
+  }
+
+  //delete product
+  deleteProduct(id:string): Observable<any>{
+    return this.http.delete('http://localhost/backend/controller/delete.php?idProduct='+id)
+  }
+
+  //create product
+  createProduct(data:Product): Observable<any> {
+    return this.http.post('http://localhost/backend/controller/create.php',{
+      description: data.descripcion,
+      price: data.precio,
+      img: data.img
+    });
+  }
+  //update data
+  updateProduct(data:Product, id:string): Observable<any> {
+    return this.http.post('http://localhost/backend/controller/create.php?idProduct='+id , {
+      description: data.descripcion,
+      price: data.precio,
+      img: data.img
+    });
   }
 }
