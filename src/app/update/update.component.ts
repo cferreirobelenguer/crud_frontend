ddd814f0a70c21d77156c566b0a6935dd2f86a8c
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-05 21:10:18 +0200

    update fetch

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Product } from '../../interfaces/product';
+import { ProductService } from 'src/services/product/product.service';
 
 @Component({
   selector: 'app-update',
@@ -8,9 +10,15 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent implements OnInit{
+  public description:string;
+  public price:number;
+  public img:string;
+  public errorMessage:string;
+  public data:Product;
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private productService: ProductService
   ) {}
   public id!: string|null;
 
@@ -26,6 +34,35 @@ export class UpdateComponent implements OnInit{
   }
 
   public handleSendData() {
-    
+    this.errorMessage= '';
+    if (this.validateInputs()) {
+      this.data = {
+        descripcion: this.description,
+        precio: this.price.toString(),
+        img: this.img
+      }
+      if(this.id != null) {
+        this.productService.updateProduct(this.data, this.id).subscribe((item) => {
+          console.log(item)
+        })
+      }
+      
+
+    } else {
+      this.errorMessage = 'Datos erróneos, no puede realizar la solicitud.'
+    }
+  }
+
+  validateInputs(): boolean {
+    // Validation inputs
+    const descriptionPattern = /[A-Za-z\s]+/;
+    const pricePattern = /[0-9]+/;
+    const imgPattern = /.+\.(png|jpg|svg)$/;
+
+    return (
+      descriptionPattern.test(this.description) &&
+      pricePattern.test(String(this.price)) &&
+      imgPattern.test(this.img)
+    );
   }
 }
