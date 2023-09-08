ac94a98e2e120f5b3a9f5051de89f833dc9dd1d7
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-08 20:37:47 +0200

    login style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(
+    private router: Router
+  ) {
 
+  }
+  public handleLogin(): void {
+
+  }
+
+  public handleRedirectRegister(): void {
+    this.router.navigate(['register'])
+  }
 }
